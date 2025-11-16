d21e1b32a88f54f8b67cb9105fabd914d1b44392
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-11-16 11:08:25 +0800

    chore: bump deps (#2)
    * chore: bump deps
    
    * use docusaurus build script from espresso repo
    
    * testing a slightly different homepage logo
    
    * slight adjustment to favicon + use katex v0.16.25

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,11 +10,15 @@ const config = {
   url: site_url,
   baseUrl: "/c-cpp/", // must have a trailing "/"
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.png",
   organizationName: "pranabdas", // Usually your GitHub org/user name.
   projectName: "c-cpp", // Usually your repo name.
   titleDelimiter: "•",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -191,7 +195,7 @@ const config = {
 
   stylesheets: [
     {
-      href: "https://pranab.pages.dev/assets/fonts/katex/0.16.22/katex.min.css",
+      href: "https://pranab.pages.dev/assets/fonts/katex/0.16.25/katex.min.css",
       type: "text/css",
     },
   ],
